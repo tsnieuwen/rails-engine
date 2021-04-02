f20d1f3bc5d64a83a33b8e99954060fbd5f5fb5f
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-04-01 18:21:16 -0600

    Add database dump to db/data directory

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# before running "rake db:seed", do the following:
+# - put the "rails-engine-development.pgdump" file in db/data/
+cmd = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails-engine_development db/data/rails-engine-development.pgdump"
+puts "Loading PostgreSQL Data dump into local database with command:"
+puts cmd
+system(cmd)
