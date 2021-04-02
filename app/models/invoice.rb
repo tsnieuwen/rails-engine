f62485ad1a356e2519ab4c4b0b65b495f717de00
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-04-02 13:20:34 -0600

    Create models and tests

---
+++ app/models/invoice.rb
@@ -0,0 +1,11 @@
+class Invoice < ApplicationRecord
+
+  belongs_to :customer
+  has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+  has_many :merchants, through: :items
+
+  enum status: [:cancelled, :in_progress, :complete]
+
+end
---
+++ app/models/transaction.rb
@@ -0,0 +1,6 @@
+class Transaction < ApplicationRecord
+
+  enum result: [:failed, :success]
+
+  belongs_to :invoice
+end
