8d7a8b0ebb5d245b06ab8f24775710a37a73558a
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-04-02 16:22:34 -0600

    Progress on merchant api

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+
+  it "sends a list of merchants" do
+
+    create_list(:merchant, 4)
+
+    get '/api/v1/merchants'
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.count).to eq(4)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
+  end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to eq(id)
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+  end
+end
