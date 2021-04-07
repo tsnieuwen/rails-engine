94c78702bbdf42e31fe10efb420aab091b09c965
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-04-06 18:04:32 -0600

    Find all items by query params complete

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,8 +1,32 @@
 class Api::V1::Items::SearchController < ApplicationController
 
   def index
-    # merchant = Merchant.where("name ILIKE ?", "%#{params[:name].downcase}%").order(name: :asc).limit(1).first
-    # render json: MerchantSerializer.new(merchant)
+    if params[:name]
+      # items = (Item.where("description ILIKE ?", "%#{params[:name].downcase}%").or(Item.where("name ILIKE ?", "%#{params[:name].downcase}%"))).where("unit_price >= ?", minimum).where("unit_price <= ?", maximum)
+      items = (Item.where("name ILIKE ?", "%#{params[:name].downcase}%")).where("unit_price >= ?", minimum).where("unit_price <= ?", maximum)
+    else
+      items = Item.where("unit_price >= ?", minimum).where("unit_price <= ?", maximum)
+    end
+    render json: ItemSerializer.new(items)
   end
+
+  private
+
+  def maximum
+    if params[:maximum]
+      params[:maximum].to_i
+    else
+      Item.maximum(:unit_price)
+    end
+  end
+
+  def minimum
+    if params[:minimum]
+      params[:minimum].to_i
+    else
+      Item.minimum(:unit_price)
+    end
+  end
+
 
 end
--- spec/requests/api/v1/item_search_spec.rb
+++ spec/requests/api/v1/item_search_spec.rb
@@ -10,12 +10,77 @@ describe "Items API" do
     item3 = FactoryBot.create(:item, name: "Bottle Cap", description: "Seals drink container", unit_price: 0.99, merchant_id: merchant1.id)
 
 
-    get '/api/v1/items/find_all', params: {name: "water" }
+    get '/api/v1/items/find_all', params: {name: "cap" }
     expect(response).to be_successful
 
     items = JSON.parse(response.body, symbolize_names: true)
-    require "pry"; binding.pry
-    # expect(merchant[:data][:attributes][:name]).to eq(merchant3.name)
+    expect(items[:data].first[:attributes][:name]).to eq(item3.name)
+  end
+
+  it "searches for all items based on minimum unit price param" do
+
+    merchant1 = FactoryBot.create(:merchant)
+    item1 = FactoryBot.create(:item, name: "Water Bottle", description: "Store your drink", unit_price: 12.34, merchant_id: merchant1.id)
+    item2 = FactoryBot.create(:item, name: "Sink", description: "Holds Water", unit_price: 100, merchant_id: merchant1.id)
+    item3 = FactoryBot.create(:item, name: "Bottle Cap", description: "Seals drink container", unit_price: 0.99, merchant_id: merchant1.id)
+
+
+    get '/api/v1/items/find_all', params: {minimum: 1.00 }
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items[:data].first[:attributes][:name]).to eq(item1.name)
+    expect(items[:data].second[:attributes][:name]).to eq(item2.name)
+  end
+
+  it "searches for all items based on maximum unit price param" do
+
+    merchant1 = FactoryBot.create(:merchant)
+    item1 = FactoryBot.create(:item, name: "Water Bottle", description: "Store your drink", unit_price: 12.34, merchant_id: merchant1.id)
+    item2 = FactoryBot.create(:item, name: "Sink", description: "Holds Water", unit_price: 100, merchant_id: merchant1.id)
+    item3 = FactoryBot.create(:item, name: "Bottle Cap", description: "Seals drink container", unit_price: 0.99, merchant_id: merchant1.id)
+
+
+    get '/api/v1/items/find_all', params: {maximum: 1.00 }
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+    expect(items[:data].first[:attributes][:name]).to eq(item3.name)
+  end
+
+  it "searches for all items based on maximum and minimum unit price param" do
+
+    merchant1 = FactoryBot.create(:merchant)
+    item1 = FactoryBot.create(:item, name: "Water Bottle", description: "Store your drink", unit_price: 12.34, merchant_id: merchant1.id)
+    item2 = FactoryBot.create(:item, name: "Sink", description: "Holds Water", unit_price: 100, merchant_id: merchant1.id)
+    item3 = FactoryBot.create(:item, name: "Bottle Cap", description: "Seals drink container", unit_price: 0.99, merchant_id: merchant1.id)
+
+
+    get '/api/v1/items/find_all', params: {minimum: 1.00, maximum: 23.00 }
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+    expect(items[:data].first[:attributes][:name]).to eq(item1.name)
+  end
+
+  it "searches for all items based on name, maximum unit price, and minimum unit price params" do
+
+    merchant1 = FactoryBot.create(:merchant)
+    item1 = FactoryBot.create(:item, name: "Water Bottle", description: "Store your drink", unit_price: 12.34, merchant_id: merchant1.id)
+    item2 = FactoryBot.create(:item, name: "Water", description: "Holds Water", unit_price: 100, merchant_id: merchant1.id)
+    item3 = FactoryBot.create(:item, name: "good Water", description: "Holds Water", unit_price: 22, merchant_id: merchant1.id)
+    item4 = FactoryBot.create(:item, name: "Bottle Cap", description: "Seals drink container", unit_price: 0.99, merchant_id: merchant1.id)
+    item5 = FactoryBot.create(:item, name: "Water Bottle", description: "Store your drink", unit_price: 12.34, merchant_id: merchant1.id)
+    item6 = FactoryBot.create(:item, name: "Bottle", description: "Store your drink", unit_price: 12.34, merchant_id: merchant1.id)
+
+    get '/api/v1/items/find_all', params: {minimum: 1.00, maximum: 23.00, name: 'water' }
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+    expect(items[:data].first[:attributes][:name]).to eq(item1.name)
+    expect(items[:data].second[:attributes][:name]).to eq(item3.name)
+    expect(items[:data].third[:attributes][:name]).to eq(item5.name)
   end
 
 end
